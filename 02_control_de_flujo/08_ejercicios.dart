a9f7848be064cfbacdba69a00ff1c00b1c5977b8
AdministradorCIFO <albertprofe@gmail.com>
2022-10-19 09:57:57 +0200

    ejercicios ciclos

--- 02_control_de_flujo/08_ejercicios.dart
+++ 02_control_de_flujo/08_ejercicios.dart
@@ -5,6 +5,7 @@ Si tu edad esta entre 18 y 20 años (incluido solamente 18) mostrar eres mayor d
 Si tu edad es menor a 18 mostrar eres menor de edad
 */
 
+/*
 import 'dart:io';
 
 void main(List<String> args) {
@@ -26,5 +27,42 @@ void main(List<String> args) {
     print("Eres menor de edad");
   } else {
     print("No he podido leer el dato");
+  }
+}
+*/
+
+/*Escribir un programa donde se identifique si el nombre del usuaro 
+se encuentra registrado*/
+
+/*
+void main(List<String> args) {
+  List<String> nombres = ["jose", "maria", "pepe"];
+  String myName = "jose";
+  bool isRegister = false;
+
+  for (String nombre in nombres) {
+    if (myName == nombre) {
+      print(myName);
+      isRegister = true;
+      break;
+    }
+  }
+  if (isRegister == true) {
+    print("Usuario registrado");
+  } else {
+    print("Usuario no registrado");
+  }
+}
+*/
+
+/*Crear una tabla de multiplicar con base 7.*/
+
+import 'dart:io';
+
+void main(List<String> args) {
+  stdout.writeln('¿Cuál es la base de la tabla de multiplicar?');
+  int base = int.tryParse(stdin.readLineSync() ?? '0') ?? 0;
+  for (var i = 0; i <= 10; i++) {
+    print('$base * $i = ${i * base}');
   }
 }
